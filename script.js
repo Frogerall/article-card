3ff69b86df24b0fd8a9295e52ae26ca724d32511
Frogerall <saliticoadrian@gmail.com>
2023-11-18 13:23:56 +0800

    added style and fix some margin and padding

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ function adjustLayout() {
  let contact = document.querySelector(".contact");
  let shareLink = document.querySelector(".share-link");
  let toolTip = document.querySelector(".tooltiptext");
+ let profileMargin = document.querySelector(".profile");
 
  if (screenWidth >= 600 && toolTip.style.display === "none") {
   toolTip.style.display = "flex";
@@ -10,10 +11,12 @@ function adjustLayout() {
   toolTip.style.display = "flex";
   contact.classList.toggle("close");
   shareLink.classList.toggle("active");
+  profileMargin.style.margin = "0rem";
  } else {
   toolTip.style.display = "none";
   contact.classList.remove("close");
   shareLink.classList.remove("active");
+  profileMargin.style.margin = "1rem";
  }
 }
 window.addEventListener("load", adjustLayout);
